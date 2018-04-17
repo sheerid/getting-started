ca38fb461305c24329ee7d242004bd7f599b2322
Sam Oberg <sam.oberg@sheerid.com>
2018-04-17 14:24:21 -0700

    create way to hide access tokens and other hashes

--- implementation/routes/index.js
+++ implementation/routes/index.js
@@ -1,7 +1,11 @@
+var bearerToken = require("../sheerid/token");
 var express = require("express");
 var router = express.Router();
 
+var token = bearerToken.getToken();
+
 router.get("/", function(req, res){
+    console.log(token);
     res.render("landing"); 
 });
 
