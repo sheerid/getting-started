aa3f14e1d57a1ad52cde9fa7a64fc6aa3897f60c
Jamie Zimmerman <jamie.zimmerman@sheerid.com>
2018-04-18 13:52:06 -0700

    handles get and post on verify

--- implementation/routes/index.js
+++ implementation/routes/index.js
@@ -2,6 +2,11 @@ var bearerToken = require("../sheerid/token");
 var express = require("express");
 var router = express.Router();
 
+const request = require('request')
+
+var token = bearerToken.getToken();
+
+
 router.get("/", function(req, res){
     res.render("landing"); 
 });
@@ -11,7 +16,24 @@ router.get("/offers", function(req, res){
 });
 
 router.get("/verify", function(req, res){
-    res.render("verify"); 
+    // query for required fields
+    var templateId = "5ad783da7584b813e77c4a6b";
+    var options = {
+        url: "https://services-sandbox.sheerid.com/rest/0.5/required_fields/template/" + templateId,
+        method: "GET",
+        json: true,
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    };
+    request(options, function(err, response, body){
+        if (err) {console.log(err)};
+        res.render("verify", body);
+    });
+});
+
+router.post("/verify", function(req, res){
+    console.log(req.body);
 });
 
 module.exports = router;
