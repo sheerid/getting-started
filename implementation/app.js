722b2e4863f7daedb95657016c0552d704376111
Sam Oberg <sam.oberg@sheerid.com>
2018-05-03 11:11:33 -0700

    allow deployed version of rainier outdoor to connect to new mlab database

--- implementation/app.js
+++ implementation/app.js
@@ -16,7 +16,11 @@ app.use("/military", militaryRoutes);
 app.use("/teacher", teacherRoutes);
 app.use("/firstresponder", firstResponderRoutes);
 
-mongoose.connect("mongodb://localhost/rainieroutdoor");
+//look for a DB URL in your envinronment variables, but if none exist,
+//just connect to your local instance of mongodb and use the rainieroutdoor db.
+//useful for heroku deployment since this variable is defined in that environment
+//and it allows your local testing to not affect the "production" database
+mongoose.connect(process.env.RAINIEROUTDOOR_DB_URL || "mongodb://localhost/rainieroutdoor");
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
     console.log("Server initialized...");
